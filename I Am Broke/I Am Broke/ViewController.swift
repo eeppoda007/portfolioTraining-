7a4bea081bc57490a725b5c141b1e9c95164fcb9
Courtney Hay <courtneyhay@student.fullsail.edu>
2020-08-22 18:13:46 -0400

    started the i am broke app

---
+++ I Am Broke/I Am Broke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Broke
+//
+//  Created by Courtney Hay on 8/22/20.
+//  Copyright © 2020 Courtney Hay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
