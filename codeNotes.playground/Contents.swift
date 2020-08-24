855892ba68c085c2a8432e39a33ead6b1f5d0507
Courtney Hay <courtneyhay@student.fullsail.edu>
2020-08-24 14:09:12 -0400

    refactored dice app

--- codeNotes.playground/Contents.swift
+++ codeNotes.playground/Contents.swift
@@ -4,3 +4,9 @@ var str = "Hello, playground"
 
 // adding an equation in a print string statement
 print("what is \(2+3) more days left to play till shool")//string interperilation
+
+// array        0        1       2
+var friends = [ "mark", "matt", "dona" ]
+// call item of the array
+friends[0]
+print(Int.random(in: 1...10))
